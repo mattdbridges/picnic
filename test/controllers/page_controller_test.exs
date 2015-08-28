0904f4e5f483f508a1093810c31889f13b6435b0
Matt Bridges <mbridges.91@gmail.com>
2015-08-28 16:00:41 -0500

    Fixed PageControllerTest

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Picnic.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "id=\"chat\""
   end
 end
